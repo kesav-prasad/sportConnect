b90cf95e6a8aa69bd436df909861a24f7cb9fd45
Kesav prasad Mahadevan <kesavprasad2007@gmail.com>
2025-12-30 15:09:03 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAUu1W8rPibsW_tS_9B6PbiruGAMB2JgiE",
+  authDomain: "sportconnect-ac7f2.firebaseapp.com",
+  projectId: "sportconnect-ac7f2",
+  storageBucket: "sportconnect-ac7f2.firebasestorage.app",
+  messagingSenderId: "3088377802",
+  appId: "1:3088377802:web:7b0e1d057f282eefa49133",
+  measurementId: "G-4PBFRC365M"
+};
